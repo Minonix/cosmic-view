ae82cec05b269c8f04dc4fe2347751c68a8c9016
titanM071 <martiniclement06@gmail.com>
2023-06-24 16:02:03 +0200

    bugg
    bug sur la fenetre nav qui ne disparai pas

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ const menuHamburger = document.querySelector(".menu-hamburger")
 const navLinks = document.querySelector(".nav-links")
 
 menuHamburger.addEventListener('click',()=>{
-  navLinks.classList.toggle('mobile-menu')
+  if (navLinks.classList.contains('mobile-menu')) {
+    navLinks.classList.remove('mobile-menu');
+    setTimeout(() => {
+      navLinks.classList.add('none');
+    }, 500);
+  } else {
+    navLinks.classList.remove('none');
+    setTimeout(() => {
+      navLinks.classList.add('mobile-menu');
+    }, 1)
+  }
 })
